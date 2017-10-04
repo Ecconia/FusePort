52a8e0f912af4c2a1e3407d8c30a44d6823ea3f6
Ecconia <eccohirsch@gmail.com>
2017-10-04 16:51:58 +0200

    Parent Command + Test Command
    TODO: Feedback/Player moduls

--- src/main/java/de/ecconia/bukkit/plugin/fuseport/FusePortPlugin.java
+++ src/main/java/de/ecconia/bukkit/plugin/fuseport/FusePortPlugin.java
@@ -2,8 +2,14 @@ package de.ecconia.bukkit.plugin.fuseport;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class FusePortPlugin extends JavaPlugin {
-	
-	
+import de.ecconia.bukkit.plugin.fuseport.command.commands.TeleportExec;
+
+public class FusePortPlugin extends JavaPlugin
+{	
+	@Override
+	public void onEnable()
+	{
+		getCommand("tp").setExecutor(new TeleportExec());
+	}
 	
 }
