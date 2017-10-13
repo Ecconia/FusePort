7355cac737ffc6085acc912e9dfa7e5e6f6c3222
Ecconia <eccohirsch@gmail.com>
2017-10-13 19:05:44 +0200

    Save languages; Replace message key if defined
    TODO: further replacing, interpreting message

--- src/main/java/de/ecconia/bukkit/plugin/fuseport/parts/feedback/FeedbackCreator.java
+++ src/main/java/de/ecconia/bukkit/plugin/fuseport/parts/feedback/FeedbackCreator.java
@@ -1,11 +1,12 @@
 package de.ecconia.bukkit.plugin.fuseport.parts.feedback;
 
 import java.io.File;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import org.bukkit.configuration.MemorySection;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -18,7 +19,7 @@ public class FeedbackCreator
 {
 	private PluginBase plugin;
 	private static final String[] langFiles = {"en"};
-	private Map<String, Object> languages;
+	private Map<String, Knot> languages;
 	
 	public FeedbackCreator(PluginBase plugin)
 	{
@@ -41,26 +42,29 @@ public class FeedbackCreator
 	
 	private void loadLangFiles()
 	{
+		languages = new HashMap<>();
 		for(String lang : langFiles)
 		{
 			FileConfiguration data = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), "languages" + File.separator + lang + ".yml"));
 			
 			Knot langTree = new Knot(data);
 			langTree.print();
-//			languages.put(lang, langTree);
+			languages.put(lang, langTree);
 		}
 	}
-	
-	
 	
 	public Feedback feedbackFromMessage(String messageKey, FPPlayer sender)
 	{
 		//TODO: Give real feedback and not some dump
+		messageKey = languages.get("en").getMessageFromKey(messageKey);
 		return new SimpleDebugFeedback(sender, messageKey);
 	}
 	
+	//TODO: extract from this class completly
+	//TODO: add extra attributes to the keys, so that one can even more hack them :P
 	private class Knot
 	{
+		//TODO: Maybe remove, to wait - time will tell
 		private Knot parent;
 		private String name;
 		private String value;
@@ -122,6 +126,42 @@ public class FeedbackCreator
 			for(int i = 0; i < childs.size(); i++)
 			{
 				childValues[i].print(suffix, i == childs.size()-1);
+			}
+		}
+		
+		public String getMessageFromKey(String key)
+		{
+			//TODO: Either block or ignore wrong usage... Such as keys: "asdf..asdf" or "" <- these just never happen on correct usage hopefully
+			//TODO: Investigate LinkedList
+			List<String> keys = new ArrayList<>(Arrays.asList(key.split("\\.")));
+			String ret = getKey(keys);
+			if(ret == null)
+			{
+				return key;
+			}
+			return ret;
+		}
+		
+		private String getKey(List<String> keys)
+		{
+			if(keys.size() == 0)
+			{
+				return value;
+			}
+			
+			String firstKey = keys.remove(0);
+			if(childs.containsKey(firstKey))
+			{
+				String ret = childs.get(firstKey).getKey(keys);
+				if(ret == null)
+				{
+					return value;
+				}
+				return ret;
+			}
+			else
+			{
+				return value;
 			}
 		}
 	}
